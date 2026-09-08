5dff25a9f92fb61050c78a481723eb9264aadb77
fdhil2582-del <fdhil2582@gmail.com>
2026-09-08 15:39:02 +0700

    Add files via upload

---
+++ nested_loop.py
@@ -0,0 +1,6 @@
+# contoh: tabel perkalian 1-3
+for i in range(1, 4) :        # loop luar: baris
+    for j in range(1, 4) :    # loop dalam: kolom
+        print(i, "x", j,"=", i*j)
+    print()   # baris kosong antar tabel
+    
